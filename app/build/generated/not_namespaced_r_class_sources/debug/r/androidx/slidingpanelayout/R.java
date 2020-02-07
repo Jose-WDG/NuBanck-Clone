ab2021995331514468e7c20070760431d299360b
José Ramalho <neto.silvacem@gmail.com>
2020-02-07 11:48:26 -0300

    adicionando dots

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/slidingpanelayout/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/slidingpanelayout/R.java
@@ -28,10 +28,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f040040;
-        public static final int notification_icon_bg_color = 0x7f040041;
-        public static final int ripple_material_light = 0x7f04004d;
-        public static final int secondary_text_default_material_light = 0x7f040050;
+        public static final int notification_action_color_filter = 0x7f040042;
+        public static final int notification_icon_bg_color = 0x7f040043;
+        public static final int ripple_material_light = 0x7f04004f;
+        public static final int secondary_text_default_material_light = 0x7f040052;
     }
     public static final class dimen {
         private dimen() {}
@@ -62,18 +62,18 @@ public final class R {
     public static final class drawable {
         private drawable() {}
 
-        public static final int notification_action_background = 0x7f060060;
-        public static final int notification_bg = 0x7f060061;
-        public static final int notification_bg_low = 0x7f060062;
-        public static final int notification_bg_low_normal = 0x7f060063;
-        public static final int notification_bg_low_pressed = 0x7f060064;
-        public static final int notification_bg_normal = 0x7f060065;
-        public static final int notification_bg_normal_pressed = 0x7f060066;
-        public static final int notification_icon_background = 0x7f060067;
-        public static final int notification_template_icon_bg = 0x7f060068;
-        public static final int notification_template_icon_low_bg = 0x7f060069;
-        public static final int notification_tile_bg = 0x7f06006a;
-        public static final int notify_panel_notification_icon_bg = 0x7f06006b;
+        public static final int notification_action_background = 0x7f060062;
+        public static final int notification_bg = 0x7f060063;
+        public static final int notification_bg_low = 0x7f060064;
+        public static final int notification_bg_low_normal = 0x7f060065;
+        public static final int notification_bg_low_pressed = 0x7f060066;
+        public static final int notification_bg_normal = 0x7f060067;
+        public static final int notification_bg_normal_pressed = 0x7f060068;
+        public static final int notification_icon_background = 0x7f060069;
+        public static final int notification_template_icon_bg = 0x7f06006a;
+        public static final int notification_template_icon_low_bg = 0x7f06006b;
+        public static final int notification_tile_bg = 0x7f06006c;
+        public static final int notify_panel_notification_icon_bg = 0x7f06006d;
     }
     public static final class id {
         private id() {}
@@ -86,26 +86,26 @@ public final class R {
         public static final int async = 0x7f08003f;
         public static final int blocking = 0x7f080042;
         public static final int chronometer = 0x7f08004b;
-        public static final int forever = 0x7f080063;
-        public static final int icon = 0x7f08006a;
-        public static final int icon_group = 0x7f08006b;
-        public static final int info = 0x7f08006f;
-        public static final int italic = 0x7f080073;
-        public static final int line1 = 0x7f080077;
-        public static final int line3 = 0x7f080078;
-        public static final int normal = 0x7f080081;
-        public static final int notification_background = 0x7f080082;
-        public static final int notification_main_column = 0x7f080083;
-        public static final int notification_main_column_container = 0x7f080084;
-        public static final int right_icon = 0x7f08008e;
-        public static final int right_side = 0x7f08008f;
-        public static final int tag_transition_group = 0x7f0800b6;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800b7;
-        public static final int tag_unhandled_key_listeners = 0x7f0800b8;
-        public static final int text = 0x7f0800b9;
-        public static final int text2 = 0x7f0800ba;
-        public static final int time = 0x7f0800bd;
-        public static final int title = 0x7f0800be;
+        public static final int forever = 0x7f080064;
+        public static final int icon = 0x7f08006b;
+        public static final int icon_group = 0x7f08006c;
+        public static final int info = 0x7f080070;
+        public static final int italic = 0x7f080074;
+        public static final int line1 = 0x7f080078;
+        public static final int line3 = 0x7f080079;
+        public static final int normal = 0x7f080082;
+        public static final int notification_background = 0x7f080083;
+        public static final int notification_main_column = 0x7f080084;
+        public static final int notification_main_column_container = 0x7f080085;
+        public static final int right_icon = 0x7f08008f;
+        public static final int right_side = 0x7f080090;
+        public static final int tag_transition_group = 0x7f0800b7;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800b8;
+        public static final int tag_unhandled_key_listeners = 0x7f0800b9;
+        public static final int text = 0x7f0800ba;
+        public static final int text2 = 0x7f0800bb;
+        public static final int time = 0x7f0800be;
+        public static final int title = 0x7f0800bf;
     }
     public static final class integer {
         private integer() {}
--- app/src/main/java/com/brq/nubanckclone/SystemAppActivity.kt
+++ app/src/main/java/com/brq/nubanckclone/SystemAppActivity.kt
@@ -2,16 +2,38 @@ package com.brq.nubanckclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
 import com.brq.nubanckclone.fragment.ItensFragment
 import com.brq.nubanckclone.fragment.login_fragment
+import kotlinx.android.synthetic.main.activity_system_app.*
 
 class SystemAppActivity : AppCompatActivity() {
+    private var vetorDeImage: ArrayList<ImageView> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_system_app)
 
+        //adicionar itens para o cliente
         var fragmentManager = supportFragmentManager
         fragmentManager.beginTransaction().add(R.id.container_fragment_system, ItensFragment()).commit()
+
+        //Adiciona dots
+
+        for( i in 0..2){
+            vetorDeImage.add(ImageView(this))
+            vetorDeImage[i].setImageDrawable(ContextCompat.getDrawable(this,R.drawable.inactive_dots))
+            var layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.setMargins(16,0,16,0)
+            container_dots.addView(vetorDeImage[i],layoutParams)
+
+        }
+        vetorDeImage[0].setImageDrawable(ContextCompat.getDrawable(this,R.drawable.active_dot))
     }
 }
